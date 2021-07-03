60132a20cfbfe443a1e17380cb9adf68b0490220
wildan bimantoro <wildan.bimantoro@meteor.id>
2021-07-03 23:20:23 +0700

    create server listerner

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const server = require('http').createServer();
+const io = require('socket.io')(server, { cors: { origin: '*' } });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const PORT = '8080';
+const CHAT_EVENT = 'newChatMessage';
 
-var app = express();
+io.on('connection', (socket) => {
+  console.log('a user connected : ', socket.id);
+  const { roomId } = socket.handshake.query;
+  
+  socket.join(roomId);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  socket.on(CHAT_EVENT, (data) => {
+    io.in(roomId).emit(CHAT_EVENT, data)
+  })
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  socket.on('disconnect', () => {
+    socket.leave(roomId);
+  });
+});
 
-module.exports = app;
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
